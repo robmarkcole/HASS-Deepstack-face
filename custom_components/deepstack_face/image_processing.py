a488051f1a37d785e04f1b7b5fb0c7988fa3d0a9
Robin Cole <robmarkcole@gmail.com>
2020-06-12 05:23:18 +0100

    reorder attributes

--- custom_components/deepstack_face/image_processing.py
+++ custom_components/deepstack_face/image_processing.py
@@ -239,8 +239,8 @@ class FaceClassifyEntity(ImageProcessingFaceEntity):
         if self._detect_only:
             attr[CONF_DETECT_ONLY] = self._detect_only
         if not self._detect_only:
-            attr["matched_faces"] = self._matched
             attr["total_matched_faces"] = len(self._matched)
+            attr["matched_faces"] = self._matched
         if self._last_detection:
             attr["last_detection"] = self._last_detection
         return attr
